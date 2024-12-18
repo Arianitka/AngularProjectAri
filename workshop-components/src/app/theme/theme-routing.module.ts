7d98365028ea849071c06b214a0c381bf879571d
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-12-18 19:22:36 +0200

    edit main component

--- workshop-components/src/app/theme/theme-routing.module.ts
+++ workshop-components/src/app/theme/theme-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { MainComponent } from "../main/main.component";
+import { MainComponent } from "./main/main.component";
+import { AddThemeComponent } from "./add-theme/add-theme.component";
 
-const routes: Routes = [{path: 'themes', component: MainComponent}];
+const routes: Routes = [{path: 'themes', component: MainComponent},
+    {path: 'add-theme', component: AddThemeComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
--- workshop-components/src/app/theme/theme.module.ts
+++ workshop-components/src/app/theme/theme.module.ts
@@ -3,17 +3,25 @@ import { CommonModule } from '@angular/common';
 import { AddThemeComponent } from './add-theme/add-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { SharedModule } from '../shared/shared.module';
+import { MainComponent } from './main/main.component';
 
 
 
 @NgModule({
   declarations: [
-    AddThemeComponent,
-    CurrentThemeComponent
+
+    CurrentThemeComponent,
+    ThemesListComponent,
+    PostsListComponent,
+    MainComponent
   ],
   imports: [
     CommonModule,
     ThemeRoutingModule,
+    SharedModule
   ]
 })
 export class ThemeModule { }
